cd5490b604f434c3d16d33173a3e9164fe9b81a9
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-04-11 14:35:20 -0300

    JwtEntryPoint - JwtProvider

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/acm/Security/jwt/JwtProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.acm.Security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author acm1ux3r0
+ */
+
+// CLASE QUE GENERA EL TOKEN con métodos de validación para verificar si está bien armado.
+
+@Component
+
+public class JwtProvider {
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}") 
+    private int expiration;
+}
+ 
