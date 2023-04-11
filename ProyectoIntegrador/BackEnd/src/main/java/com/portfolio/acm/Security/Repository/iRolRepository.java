d05251954db38e4c4630b217ebf00b8fbf01d2bb
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-04-11 12:17:57 -0300

    iRolRepository, iUsuarioRepository, RolService, UserDetailsImpl, UsuarioService
    Creación de Package, Clases, Interfaces, implementaciones etc

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/acm/Security/Repository/iRolRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.acm.Security.Repository;
+
+import com.portfolio.acm.Security.Entity.Rol;
+import com.portfolio.acm.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author acm1ux3r0
+ */
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/acm/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.acm.Security.Repository;
+
+import com.portfolio.acm.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author acm1ux3r0
+ */
+
+@Repository
+public interface iUsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+}
