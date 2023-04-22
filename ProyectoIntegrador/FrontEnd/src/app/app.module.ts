6399f3ba4619d7f2cbdd77522115ecd4b95d8924
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-04-22 00:30:30 -0300

    login.component.html OK

--- ProyectoIntegrador/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador/FrontEnd/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { interceptorProvider } from './service/interceptor-service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
              /*  <<<<<<<<<<<<<<< Specify ng-circle-progress as an import >>>>>>>>>>>>>> */
     NgCircleProgressModule.forRoot({ /* Aquí se colocarían los valores por defecto */}),
     HttpClientModule
