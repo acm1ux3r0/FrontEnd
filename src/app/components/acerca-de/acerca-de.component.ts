e18bdf399aeec0912e120598c9af6e6d10250066
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-05-21 03:10:41 -0300

    Actualización de los componentes  "acerca-de" y de "experiencia"

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
 
+
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
@@ -9,13 +10,15 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 
 export class AcercaDeComponent implements OnInit {
-  /*persona: persona = new persona("","","");*/ 
-  persona: persona = new persona("Antonio", "Lucero", "../../../assets/perfilPhoto.jpg");
+  persona: persona = new persona("","","");
+  //persona: persona = new persona("Antonio", "Lucero", "../../../assets/perfilPhoto.jpg");
 
   constructor(public personaService: PersonaService){ }
 
   ngOnInit(): void{
     this.personaService.getPersona().subscribe(
+                                    /* El método suscribe está pendiente de los 
+                                    eventos del "observable" */
       data => {
         this.persona = data
       }
